99728634c924fb6315f6237dc418813c356ee2df
Filipe Melo <filipexpert63@gmail.com>
2021-08-11 14:35:22 -0300

    Finalização da primeira parte de checkWin

--- script/script.js
+++ script/script.js
@@ -85,15 +85,7 @@ const checkWin = (value) => {
   }
   return false
 }
-// function checkWin() {
 
-
-//     checkHorizontal(i, j)
-//     checkVertical(i, j)
-//     checkDiagonal(i, j)
-//     checkDiagonalReverse(i, j)
-
-// }
 
 
 function checkDraw() {
@@ -112,99 +104,6 @@ function showResult() {
 
 
 /* FUNÇÕES FILIPE POSSIBILIDADES */
-
-
-
-
-// let possibleResults = []
-// let table = 42
-// let victoryCondition = 4
-
-// teste token
-
-// // ---------------   HORIZONTAL  --------------------
-    // const generatorH = (table, value) => {
-    //     for(let i = 1; i <= table; i+=7){
-    //         let init = i
-    //             for(let j = 0; j < victoryCondition; j++){
-    //                 if(init + j === value){
-    //                     possibleResults.push(multiplyH(value))
-    //                 }
-    //             }
-    //     }
-    // }
-    // const multiplyH  = (value) => {
-    //     let output = []
-    //     for(let i = 0; i < victoryCondition; i++){
-    //         output.push(value + i)
-    //     }
-    //     return output
-    // }
-    // for(let i = 0; i < table; i++){
-    //     generatorH(table, i)
-    // }
-
-    // //=----- VERTICAL ---------
-    // const multiplyV = (value) => {
-    //     let output = [value]
-    //     for(let i = 0; i < 3; i++){
-    //         output.push(value += 7)
-    //     }
-    //     return output
-    // }
-    // for(let i = 1; i <= table / 2; i++){
-    //     possibleResults.push(multiplyV(i))
-    // }
-    // // ----------DIAGONAL--------------
-    // const generatorDiagonal = (table, valor) => {
-    //     for(let i = 1; i <= table; i+=7){
-    //         let init = i
-    //             for(let j = 0; j < victoryCondition; j++){
-    //                 if(init + j === valor){
-    //                     possibleResults.push(multiplyDiagonal(valor))
-    //                 }
-    //             }
-    //     }
-    // }
-    // const multiplyDiagonal =  (value) => {
-    //     let output = [value]
-    //     for(let i = 0; i < 3; i++){
-    //         output.push(value += 8)
-    //     }
-    //     return output
-    // }
-    // for(let i = 0; i <= 18; i++){
-    //     generatorDiagonal(table, i)
-    // }
-    // //-------------Reverse--------------
-    // const generatorDiagonalReverse = (table, valor) => {
-    //     for(let i = 20; i <= table; i++){
-    //         if(i % 7 === 0){
-    //             let init = i + 1
-    //             for(let j = 0; j < victoryCondition; j++){
-    //                 if(init + j === valor){
-    //                     possibleResults.push(multiplicaDiagonalReverse(valor))
-    //                 }
-    //             }
-    //         } 
-    //     }
-    // }
-    // const multiplicaDiagonalReverse =  (value) => {
-    //     let output = [value]
-    //     for(let i = 0; i < 3; i++){
-    //         output.push(value -= 6)
-    //     }
-    //     return output
-    // }
-    // for(let i = 0; i <= table; i++){
-    //     generatorDiagonalReverse(table, i)
-    // }
-
-
-
-// -----------------------------
-//-   COMBINAÇÕES POR ÍNDICE
-//------------------------------
 
 
     const generatorTableID = () => {
