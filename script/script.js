4ecdec7e936aca3baae4742c42522c11eee63bd2
Nicky <nicollyporfirio12@gmail.com>
2021-08-12 16:24:22 -0300

    Código adicionado novamente

--- script/script.js
+++ script/script.js
@@ -1,31 +1,30 @@
-
 /* COMEÇO DECLARAÇÃO VARIÁVEIS */
 
-const tableID = []
-let colNumber = 7
-let rowNumber = 6
-let combinationCircle = 4
-let maxColCombination = colNumber - combinationCircle + 1
-let maxRowCombination = rowNumber - combinationCircle + 1
-let maxDiagonalDownCombination = ""//?
-let maxDiagonalUpCombination = ""// ?
-let possibilities = []
-let playerOneChoices = []
-let playerTwoChoices = []
+const tableID = [];
+let colNumber = 7;
+let rowNumber = 6;
+let combinationCircle = 4;
+let maxColCombination = colNumber - combinationCircle + 1;
+let maxRowCombination = rowNumber - combinationCircle + 1;
+let maxDiagonalDownCombination = ""; //?
+let maxDiagonalUpCombination = ""; // ?
+let possibilities = [];
+let playerOneChoices = [];
+let playerTwoChoices = [];
 
-let currentColumn
+let currentColumn;
 
-let currentPlayer = "player1"
+let currentPlayer = "player1";
 
 let mainGame = document.querySelector(".main-game");
 
 const result = document.getElementById("resultModal");
 const text = document.getElementById("textResult");
-const again = document.getElementById("again")
+const again = document.getElementById("again");
 
-let columns
+let columns;
 
-let diskCreated = false
+let diskCreated = false;
 
 /* FIM DECLARAÇÃO VARIÁVEIS */
 
@@ -34,235 +33,72 @@ let diskCreated = false
 tableGenerator();
 
 function game(event) {
+  currentColumn = event.currentTarget;
 
-  currentColumn = event.currentTarget
+  diskCreated = false;
 
-  diskCreated = false
-  
   if (checkViability()) {
     createDisk();
-    diskCreated = true
+    diskCreated = true;
   }
-  
 
   if (checkWin()) {
     showResult("win");
   }
 
-  if (checkDraw() & checkWin() === false) {
+  if (checkDraw() & (checkWin() === false)) {
     showResult("draw");
   }
 
-  if(diskCreated) {
+  if (diskCreated) {
     switchPlayer();
   }
-
 }
 
+let mainGame = document.querySelector(".main-game");
 
-function tableGenerator() {
-
-  for (let i = 0; i < 7; i++) {
-    let column = document.createElement("div");
-    column.classList.add("column");
-    column.id = `column_${i}`;
-    mainGame.appendChild(column);
-    for (let j = 0; j < 6; j++) {
-      let square = document.createElement("div");
-      square.classList.add("square");
-      square.dataset.square_id = j + i * 6 + 1;
-      column.appendChild(square);
-    }
-  }
-
-  columns = document.querySelectorAll(".column");
-  columns.forEach((element) => {
-    element.addEventListener("click", game);
-  });
-}
-
-
-function checkViability() {
-
-  let squareArray = currentColumn.querySelectorAll(".disk")
-
-  if (squareArray.length < 6) {
-    return true
-  } else {
-    return false
-  }
-  
-}
-
-
-function switchPlayer() {
-  if (currentPlayer === "player1") {
-    currentPlayer = "player2";
-  } else {
-    currentPlayer = "player1";
+for (let i = 0; i < 7; i++) {
+  let column = document.createElement("div");
+  column.classList.add("column");
+  column.id = `column_${i}`;
+  mainGame.appendChild(column);
+  for (let j = 0; j < 6; j++) {
+    let square = document.createElement("div");
+    square.classList.add("square");
+    square.id = j + i * 7;
+    column.appendChild(square);
   }
 }
-
-
 function createDisk() {
   let disk = document.createElement("div");
   disk.classList.add("disk");
-  let currentDisks = currentColumn.querySelectorAll(".disk");
-  let diskCount = currentDisks.length;
-  let currentSquares = currentColumn.querySelectorAll(".square")
-  let squareCount = currentSquares.length
-  let destinySquare = currentColumn.querySelector(
-    `.square:nth-child(${squareCount - diskCount})`);
+  mainGame.appendChild(disk);
+}
+createDisk();
+let player = document.querySelector(".disk");
+
+let currentPlayer = "player1";
+function swichPlayer() {
   if (currentPlayer === "player1") {
-    disk.classList.add("player1");
-    destinySquare.appendChild(disk);
-    playerOneChoices.push(Number(destinySquare.dataset.square_id));
+    player.classList.add(".player1");
+    currentPlayer = "player2";
   } else {
-    disk.classList.add("player2");
-    destinySquare.appendChild(disk);
-    playerTwoChoices.push(Number(destinySquare.dataset.square_id));
+    currentPlayer = "player1";
+    player.classList.add(".player2");
   }
 }
 
+let columns = document.querySelectorAll(".column");
 
-const checkWin = () => {
-      
-  for( let i = 0; i < possibilities.length; i++){
-    if (win(playerOneChoices, possibilities[i]) === true){
-        return true
-    }
-    if (win(playerTwoChoices, possibilities[i]) === true){
-      return true
-    }
-  }
-  return false
+columns.forEach((element) => {
+  element.addEventListener("click", pickColumn);
+});
+
+function pickColumn(event) {
+  let selectColumn = event.currentTarget;
+
+  let id = document.getElementById("column_2");
+  for (let i = id[length - 1]; i >= 0; i--) {}
+  console.log(selectColumn.lastChild);
+  selectColumn.lastChild.style.backgroundColor = "red";
 }
-
-
-const showResult = (results) => {
-
-  if(results === "win") {
-    text.innerHTML = `${currentPlayer}!!! O mizeravi é um gênio `
-  }  
-
-  else {
-    text.innerHTML = `Poxa :( infelizmente dessa vez você não conseguir nos ajudar. Tente novamente`
-  }
-
-  result.classList.remove("hidden");
-}
-
-again.addEventListener("click", resetGame);
-
-
-function resetGame() {
-  mainGame.innerText = ""
-  tableGenerator()
-  currentPlayer = "player1"
-  playerOneChoices = []
-  playerTwoChoices = []
-  result.classList.add("hidden")
-}
-
-
-const generatorTableID = () => {
-  const mult = colNumber * rowNumber
-  for(let i = 1; i <= mult; i += rowNumber){
-    let arr = [];
-    let count = 0
-    for(let j = 0; j < rowNumber; j++){
-      arr.push(i + count)
-      count++
-    }
-    tableID.push(arr)
-  }
-}
-generatorTableID()
-
-
-const horizontalGenerator = () => {
-  for (let i = 0; i < colNumber; i++) {
-    for (let r = 0; r < maxRowCombination; r++) {
-      let combination = []
-      for (let j = 0; j < combinationCircle; j++) {
-        combination.push(tableID[i][j + r])
-      }
-      possibilities.push(combination)
-    }
-  }
-}
-
-const checkDraw = () => {
-
-  let squareCount = document.querySelectorAll(".square");
-  let diskCount = document.querySelectorAll(".disk");
-
-  return squareCount.length === diskCount.length;
-}
-
-const diagonalDownGenerator = () => {
-  for (let i = 0; i < 4; i++) {
-    for (let r = 0; r < 3; r++) {
-      let combination = []
-      for (let j = 0; j < combinationCircle; j++) {
-        combination.push(tableID[i + j][j + r])
-      }
-      possibilities.push(combination)
-    }
-  }
-}
-
-const verticalGenerator = () => {
-  for (let i = 0; i < rowNumber; i++) {
-    for (let r = 0; r < maxColCombination; r++) {
-      let combination = []
-      for (let j = 0; j < combinationCircle; j++) {
-        combination.push(tableID[j + r][i])
-      }
-      possibilities.push(combination)
-    }
-  }
-}
-
-const diagonalUpGenerator = () => {
-  for (let i = 3; i < 7; i++) {
-    for (let r = 0; r < 3; r++) {
-      let combination = []
-      for (let j = 0; j < combinationCircle; j++) {
-        combination.push(tableID[i - j][j + r])
-      }
-      possibilities.push(combination)
-    }
-  }
-}
-
-
-const win = (player, condicao) => {
-  let count = 0
-  for (let i = 0; i < player.length; i++){
-    for ( let j = 0; j < condicao.length; j++ ){
-      if(player[i] === condicao[j]){
-        count++
-      }
-    }
-  }
-  if(count === combinationCircle){
-    return true
-  }
-  return false
-}
-
-
-const possibilitiesGenerator = () => {
-  horizontalGenerator()
-  verticalGenerator()
-  diagonalDownGenerator()
-  diagonalUpGenerator()
-
-}
-possibilitiesGenerator()
-
-
-
-/* FIM DAS FUNÇÕES */
-
